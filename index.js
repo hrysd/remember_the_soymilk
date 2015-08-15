1e5c19a1b23e22a2f751a714b8906e1a80d6d2b1
Hiroshi Yoshida <hrysd22@gmail.com>
2015-08-15 09:17:00 +0900

    Use `menubar`

--- index.js
+++ index.js
@@ -3,16 +3,4 @@
 const App           = require('app'),
       BrowserWindow = require('browser-window');
 
-App.on('ready', function() {
-  let mainWindow = new BrowserWindow({width: 980, height: 1000});
-
-  mainWindow.loadUrl(`file://${__dirname}/index.html`);
-
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
-});
-
-App.on('window-all-closed', function() {
-  App.quit();
-});
+const MenuBar = require('menubar')({dir: __dirname, width: 980});
